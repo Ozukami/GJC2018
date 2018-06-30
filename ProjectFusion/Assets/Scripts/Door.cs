f4a8c830b3e88439b6cde756038c3aa146828bcb
Arthur Poisson <apoisson@student.42.fr>
2018-06-30 07:53:15 +0200

    DoorCollision

--- ProjectFusion/Assets/Scripts/Door.cs
+++ ProjectFusion/Assets/Scripts/Door.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour {
-
     private Animator _animator;
-    
+    private Collider2D _collider;
+
     void Start () {
+        _collider = GetComponent<Collider2D>();
         _animator = GetComponent<Animator>();
     }
-    
+
     private void Update () {
-        if (Input.GetKeyDown(KeyCode.O))
+        if (Input.GetKeyDown(KeyCode.O)) {
             _animator.SetTrigger("open");
-        if (Input.GetKeyDown(KeyCode.F))
+            _collider.enabled = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F)) {
             _animator.SetTrigger("close");
+            _collider.enabled = true;
+        }
     }
-    
 }
