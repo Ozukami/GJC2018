f3db1e5c41ec102bdd5386cbee9d508229d2fd15
Arthur Poisson <apoisson@student.42.fr>
2018-06-30 07:03:54 +0200

    Animations

--- ProjectFusion/Assets/Scripts/PlayerController.cs
+++ ProjectFusion/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public enum Element {
     fire, earth, water, wind
 }
 
+public enum Orientation {
+    up = 0, down = 1, right = 2, left = 3
+}
+
 public class PlayerController : MonoBehaviour {
     
     [SerializeField]
@@ -15,26 +19,72 @@ public class PlayerController : MonoBehaviour {
     private bool isDead = false;
 
     private int life = 10;
+    private int orientation;
+
+    private Animator _animator;
+    private Rigidbody2D _rb2d;
 
     // Use this for initialization
     void Start () {
+        _animator = GetComponent<Animator>();
+        _rb2d = GetComponent<Rigidbody2D>();
         Debug.Log("I'm alive");
     }
     
     // Update is called once per frame
     void Update () {
+        
+    }
+    
+    void FixedUpdate () {
         InputHandler();
+        OrientPlayer();
     }
 
     void InputHandler () {
+        /*
         if (Input.GetKey(KeyCode.LeftArrow)) {
+            _animator.SetBool("isWalking", true);
+            _animator.SetInteger("orientation", 2);
+            GetComponent<SpriteRenderer>().flipX = true;
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         } else if (Input.GetKey(KeyCode.UpArrow)) {
+            _animator.SetBool("isWalking", true);
+            _animator.SetInteger("orientation", 1);
             transform.Translate(Vector3.up * Time.deltaTime * speed);
         } else if (Input.GetKey(KeyCode.RightArrow)) {
+            _animator.SetBool("isWalking", true);
+            _animator.SetInteger("orientation", 2);
+            GetComponent<SpriteRenderer>().flipX = false;
             transform.Translate(Vector3.right * Time.deltaTime * speed);
         } else if (Input.GetKey(KeyCode.DownArrow)) {
+            _animator.SetBool("isWalking", true);
+            _animator.SetInteger("orientation", 0);
             transform.Translate(Vector3.down * Time.deltaTime * speed);
+        } else {
+            GetComponent<SpriteRenderer>().flipX = false;
+            _animator.SetBool("isWalking", false);
+        }
+        */
+        float axisX = Input.GetAxis("Horizontal");
+        float axisY = Input.GetAxis("Vertical");
+        if (axisX != 0 || axisY != 0) {
+            _animator.SetBool("isWalking", true);
+            _animator.SetFloat("orientationX", axisX);
+            _animator.SetFloat("orientationY", axisY);
+            GetComponent<SpriteRenderer>().flipX = (axisX < 0) ? true : false;
+        } 
+        else
+            _animator.SetBool("isWalking", false);
+        _animator.SetFloat("axisX", axisX);
+        _animator.SetFloat("axisY", axisY);
+        transform.Translate(Vector3.right * axisX * Time.deltaTime * speed);
+        transform.Translate(Vector3.up * axisY * Time.deltaTime * speed);
+    }
+
+    void OrientPlayer () {
+        switch (orientation) {
+                
         }
     }
 
