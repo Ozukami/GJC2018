77799b8d635474943df8d6a7ef33f465a60e2166
Quentin Maujean <qumaujea@student.42.fr>
2018-07-01 00:46:25 +0200

    GameManager start

---
+++ ProjectFusion/Assets/Scripts/EndRoom.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndRoom : MonoBehaviour
+{
+    private int colliders;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        colliders++;
+
+        
+        print(colliders);
+        if (colliders == 4)
+        {
+            GetComponentInParent<GameManager>().SetState(GameStates.LevelSelection);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        colliders--;
+    }
+}
---
+++ ProjectFusion/Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public enum GameStates
+{
+    Intro,
+    Mainmenu,
+    LevelSelection
+}
+
+public class GameManager : MonoBehaviour
+{
+    private BoxCollider2D endRoom;
+    public bool paused;
+    private GameStates currentState;
+    public static GameManager Gm = null;
+
+    void Awake()
+    {
+        if (Gm == null)
+        {
+            Gm = this;
+        }
+        else if (Gm != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        endRoom = GetComponentInChildren<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(currentState);
+    }
+
+    public GameStates SetState(GameStates state)
+    {
+        if (currentState != state)
+        {
+            currentState = state;
+
+            switch (state)
+            {
+                case GameStates.Intro:
+                    
+                    break;
+                case GameStates.Mainmenu:
+                    
+                    break;
+                case GameStates.LevelSelection:
+                    
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return currentState;
+    }
+}
