84c0a7d6bf9dcf5092cbb4d756c4a00b63e10f1b
Jeremykaito <jh152891@edu.ece.fr>
2018-07-01 19:24:58 +0200

    boss

---
+++ ProjectFusion/Assets/Scripts/Enemy/Boss.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+
+    public GameObject[] patrolPoints;
+    public string element;
+    public float moveSpeed;
+    private int currentPoint;
+    private Animator _animator;
+    private Vector3 old_pos;
+    float axisX = 0;
+    float axisY = 0;
+    public float life = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Fire" || collision.gameObject.tag == "Water" || collision.gameObject.tag == "Wind" || collision.gameObject.tag == "Earth" )
+        {
+            life--;
+            if(life<=0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
