41b04a3e74448690df2e049413921f5cd2128387
Quentin Maujean <qumaujea@student.42.fr>
2018-07-01 14:10:33 +0200

    Good doors

---
+++ ProjectFusion/Assets/Scripts/ElementDoor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementDoor : MonoBehaviour
+{
+    [SerializeField] private Sprite open;
+    [SerializeField] private Sprite close;
+    [SerializeField] private ElementType _type;
+    private Collider2D _collider;
+
+    // Use this for initialization
+    void Start()
+    {
+        _collider = GetComponentInChildren<BoxCollider2D>();
+        _collider.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.CompareTag(_type.ToString()))
+            Physics2D.IgnoreCollision(col.gameObject.GetComponent<Collider2D>(), _collider);
+    }
+}
