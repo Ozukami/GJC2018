dd3de6c0dd1e2db142803c09396ca9023b8b6bdf
Arthur Poisson <apoisson@student.42.fr>
2018-07-01 14:57:29 +0200

    PlayerProj

---
+++ ProjectFusion/Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour {
+    public float fireSpeed;
+    public float VelX;
+    public float VelY;
+
+    // Use this for initialization
+    void Start () {
+        transform.right = new Vector2(VelX, VelY);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(Vector3.right * Time.deltaTime * fireSpeed);
+        Destroy(gameObject, 5f);
+    }
+
+    private void OnTriggerEnter2D (Collider2D other) {
+        
+    }
+}
